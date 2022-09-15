81cc048debaa2b9d97ee6965ed2d689bff11b6ce
HimavanthReddyPallapu <113073979+HimavanthReddyPallapu@users.noreply.github.com>
2022-09-15 01:16:22 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,7 +1,35 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(n)
+// Space Complexity : Constant
+// Did this code successfully run on Leetcode : YES
+// Any problem you faced while coding this : NO
 
 
 // Your code here along with comments explaining your approach
+
+class Solution {
+    public int longestPalindrome(String s) {
+        if(s.length()==0) {return 0;}
+        int len=0;
+        HashSet<Character> hs=new HashSet<>();
+        for(int i=0;i<s.length();i++)
+        {
+            char c=s.charAt(i);
+            if(hs.contains(c))
+            {
+                len=len+2;
+                hs.remove(c);
+            }
+            else
+            {
+                hs.add(c);
+            }
+            
+            
+        }
+        if(hs.size()>0)
+        {
+            len++;
+        }
+        return len;
+    }
+}
